c063d12341885d9e6a8a693b5be1d633d3db3deb
David Morse <morse012@umn.edu>
2026-01-19 20:47:46 -0600

    Attempted conversion of Rpc::Polymer (unfinished)

--- src/rpc/solvers/Polymer.cpp
+++ src/rpc/solvers/Polymer.cpp
@@ -5,13 +5,21 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "Polymer.tpp"
+#include "Polymer.h"
+#include <rpc/solvers/Block.h>
+#include <rpc/solvers/Propagator.h>
+#include <prdc/cpu/RField.h>
+#include <pscf/solvers/PolymerTmpl.tpp>
+#include <pscf/chem/PolymerModel.h>
+
+#include <rp/solvers/Polymer.tpp>
 
 namespace Pscf {
-
-   template class PolymerTmpl< Rpc::Block<1>, Rpc::Propagator<1> >;
-   template class PolymerTmpl< Rpc::Block<2>, Rpc::Propagator<2> >;
-   template class PolymerTmpl< Rpc::Block<3>, Rpc::Propagator<3> >;
+   namespace Rp {
+      template class Polymer< 1, Rpc::Types<1> >;
+      template class Polymer< 2, Rpc::Types<2> >;
+      template class Polymer< 3, Rpc::Types<3> >;
+   }
 
    namespace Rpc {
       template class Polymer<1>;
--- src/rpc/solvers/Polymer.h
+++ src/rpc/solvers/Polymer.h
@@ -8,31 +8,8 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include <pscf/solvers/PolymerTmpl.h>    // base class template
-#include <util/containers/FSArray.h>     // member template
-
-// Forward declarations
-namespace Util {
-   template <typename T> class DArray;
-}
-namespace Pscf {
-   namespace Rpc {
-      template <int D> class Block;
-      template <int D> class Propagator;
-   }
-   namespace Prdc {
-      namespace Cpu {
-         template <int D> class RField;
-      }
-   }
-}
-
-// Explicit instantiation declarations for base classes
-namespace Pscf {
-   extern template class PolymerTmpl< Rpc::Block<1>, Rpc::Propagator<1> >;
-   extern template class PolymerTmpl< Rpc::Block<2>, Rpc::Propagator<2> >;
-   extern template class PolymerTmpl< Rpc::Block<3>, Rpc::Propagator<3> >;
-}
+#include <rp/solvers/Polymer.h>    // base class template
+#include <rpc/system/Types.h>      // base class template
 
 namespace Pscf {
 namespace Rpc {
@@ -44,113 +21,26 @@ namespace Rpc {
    /**
    * Descriptor and solver for one polymer species.
    *
-   * The phi() and mu() accessor functions, which are inherited from
-   * PolymerSpecies, return the value of phi (spatial average volume
-   * fraction of a species) or mu (species chemical potential) computed
-   * in the last call of the compute() function. If the ensemble for this
-   * species is closed, phi is read from the parameter file and mu is
-   * computed. If the ensemble is open, mu is read from the parameter
-   * file and phi is computed.
-   *
-   * The block concentrations stored in the constituent Block<D> objects
-   * contain the block concentrations (i.e., volume fractions) computed in
-   * the most recent call of the compute function. These can be accessed
-   * using the Block<D>::cField() function.
+   * This class is simply a named instantiation of the base class 
+   * template Rp::Polymer. Please see documentation of the base class
+   * for details.
    *
    * \ref user_param_polymer_sec "Manual Page"
    *
    * \ingroup Rpc_Solver_Module
    */
    template <int D>
-   class Polymer : public PolymerTmpl< Block<D>, Propagator<D> >
+   class Polymer : public Rp::Polymer<D, Types<D>>
    {
 
    public:
 
-      // Public type name aliases
 
       /// Base class, partial template specialization.
       using Base = PolymerTmpl< Block<D>, Propagator<D> >;
 
-      /// Block type, for a block within a block polymer.
-      using BlockT = Block<D>;
-
-      /// Propagator type, for one direction within a block.
-      using PropagatorT = Propagator<D>;
-
-      // Public member functions
-
-      /**
-      * Constructor.
-      */
-      Polymer();
-
-      /**
-      * Destructor.
-      */
-      ~Polymer();
-
-      /**
-      * Set the number of unit cell parameters.
-      *
-      * \param nParam  the number of unit cell parameters
-      */
-      void setNParams(int nParam);
-
-      /**
-      * Clear all data that depends on unit cell parameters.
-      *
-      * This function should be called after each change in the unit cell.
-      * It calls Block<D>::clearUnitCellData() for all blocks in this
-      * polymer.
-      */
-      void clearUnitCellData();
-
-      /**
-      * Compute MDE solutions and block concentrations.
-      *
-      * This function sets up w-fields in the MDE solvers for all blocks,
-      * calls the base class PolymerTmpl solve function to solve the MDE
-      * for all blocks, and then computes concentrations associated with
-      * all blocks. On return, the associated Block objects all contain
-      * propagator solutions and block volume fraction fields, while q and
-      * phi or mu are set to new values.
-      *
-      * The parameter phiTot is only relevant to problems such as thin
-      * films in which the material is excluded from part of the unit
-      * cell by imposing an inhogeneous constraint on the sum of the
-      * monomer concentrations (i.e., a "mask").
-      *
-      * \param wFields array of chemical potential fields.
-      * \param phiTot  volume fraction of unit cell occupied by material
-      */
-      void compute(DArray< RField<D> > const & wFields,
-                   double phiTot = 1.0);
-
-      /**
-      * Compute SCFT stress contribution from this polymer species.
-      *
-      * This function computes contributions from this species to the
-      * derivatives of SCFT free energy per monomer with respect to unit
-      * cell parameters and stores the values. It requires that the MDE
-      * has been solved for all blocks prior to entry, and so must be
-      * called after the compute function.
-      */
-      void computeStress();
-
-      /**
-      * Get the precomputed contribution to stress from this species.
-      *
-      * Get the contribution from this polymer species to the derivative of
-      * free energy per monomer with respect to unit cell parameter n, as
-      * computed by the most recent call to computeStress().
-      *
-      * \param n  unit cell parameter index
-      */
-      double stress(int n) const;
-
+      #if 0
       // Inherited public member functions
-
       using Base::edge;
       using Base::block;
       using Base::propagator;
@@ -169,25 +59,9 @@ namespace Rpc {
       using Species<double>::ensemble;
       using Species<double>::setPhi;
       using Species<double>::setMu;
-
-   private:
-
-      /// Stress contribution from this polymer species
-      FSArray<double, 6> stress_;
-
-      /// Number of unit cell parameters
-      int nParam_;
-
-      // Restrict access to inherited functions
-      using Base::solve;
-      using Species<double>::setQ;
+      #endif
 
    };
-
-   /// Get stress component n.
-   template <int D> inline
-   double Polymer<D>::stress(int n) const
-   {  return stress_[n]; }
 
    // Explicit instantiation declarations
    extern template class Polymer<1>;
@@ -196,4 +70,14 @@ namespace Rpc {
 
 } // namespace Rpc
 } // namespace Pscf
+
+// Explicit instantiation declarations for base classes
+namespace Pscf {
+   namespace Rp {
+      extern template class Polymer<1, Rpc::Types<1> >;
+      extern template class Polymer<2, Rpc::Types<2> >;
+      extern template class Polymer<3, Rpc::Types<3> >;
+   } 
+}
+
 #endif
