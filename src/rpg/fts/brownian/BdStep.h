828c923284fac40bfa404410ca4c4ab1d367fb81
David Morse <morse012@umn.edu>
2026-02-01 23:43:28 -0600

    Deriving Rpg::BdStep from Rp:BdStep

--- src/rpg/fts/brownian/BdStep.h
+++ src/rpg/fts/brownian/BdStep.h
@@ -8,22 +8,13 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include <util/param/ParamComposite.h>
-//#include <pscf/cuda/CudaVecRandom.h>
-
-namespace Pscf {
-   class CudaVecRandom;
-   namespace Rpg {
-      template <int D> class System;
-      template <int D> class BdSimulator;
-   }
-}
+#include <rp/fts/brownian/BdStep.h>
+#include <rpg/system/Types.h>
 
 namespace Pscf {
 namespace Rpg {
 
    using namespace Util;
-
 
    /**
    * BdStep is an abstract base class for Brownian dynamics steps.
@@ -33,7 +24,7 @@ namespace Rpg {
    * \ingroup Rpg_Fts_Brownian_Module
    */
    template <int D>
-   class BdStep : public ParamComposite
+   class BdStep : public Rp::BdStep<D, Types<D> >
    {
 
    public:
@@ -45,121 +36,22 @@ namespace Rpg {
       */
       BdStep(BdSimulator<D>& simulator);
 
-      /**
-      * Destructor.
-      *
-      * Empty default implementation.
-      */
-      virtual ~BdStep();
-
-      /**
-      * Read required parameters from file.
-      *
-      * Empty default implementation.
-      */
-      virtual void readParameters(std::istream &in);
-
-      /**
-      * Setup before the beginning of each simulation run.
-      */
-      virtual void setup();
-
-      /**
-      * Take a single Brownian dynamics step.
-      * 
-      * \return true if converged, false if failed to converge.
-      */
-      virtual bool step() = 0;
-      
-      /**
-      * Decide whether cc fields need to be saved for move.
-      * The default implementation is false.
-      */
-      virtual bool needsCc()
-      {  return false; }
-      
-      /**
-      * Decide whether dc fields need to be saved for move.
-      * The default implementation is false.
-      */
-      virtual bool needsDc()
-      { return true; }
-      
-      /**
-      * Log output timing results 
-      */
-      virtual void outputTimers(std::ostream& out);
-      
-      /**
-      * Clear timers 
-      */
-      virtual void clearTimers();
-
-      // Accessor Functions
-
-      /**
-      * Output statistics for this move (at the end of simulation)
-      */
-      virtual void output();
-
-   protected:
-
-      /**
-      * Get parent System object.
-      */
-      System<D>& system();
-
-      /**
-      * Get parent BdSimulator object.
-      */
-      BdSimulator<D>& simulator();
-
-      /**
-      * Get Random number generator of parent System.
-      */
-      CudaVecRandom& vecRandom();
-
-   private:
-
-      /// Pointer to parent BdSimulator object
-      BdSimulator<D>* simulatorPtr_;
-
-      /// Pointer to parent System object
-      System<D>* systemPtr_;
-
-      /// Pointer to random number generator
-      CudaVecRandom  *vecRandomPtr_;
-
    };
 
-   // Protected inline methods
-
-   /*
-   * Get parent System object.
-   */
-   template <int D>
-   inline System<D>& BdStep<D>::system()
-   {  return *systemPtr_; }
-
-   /*
-   * Get parent BdSimulator object.
-   */
-   template <int D>
-   inline BdSimulator<D>& BdStep<D>::simulator()
-   {  return *simulatorPtr_; }
-
-   /*
-   * Get Random number generator.
-   */
-   template <int D>
-   inline CudaVecRandom& BdStep<D>::vecRandom()
-   {  return *vecRandomPtr_; }
-
-   // Explicit instantiation declarations
-   extern template class BdStep<1>;
-   extern template class BdStep<2>;
-   extern template class BdStep<3>;
-
 }
+}
+
+// Explicit instantiation declarations
+namespace Pscf {
+   namespace Rp {
+      extern template class BdStep< 1, Rpg::Types<1> >;
+      extern template class BdStep< 2, Rpg::Types<2> >;
+      extern template class BdStep< 3, Rpg::Types<3> >;
+   }
+   namespace Rpg {
+      extern template class BdStep<1>;
+      extern template class BdStep<2>;
+      extern template class BdStep<3>;
+   }
 }
 #endif
