e1184b946d6e912dd335f129d6c88478e19c839e
David Morse <morse012@umn.edu>
2026-02-16 16:40:07 -0600

    Deriving Rpg::LMBdStep from Rp template

--- src/rpg/fts/brownian/LMBdStep.cu
+++ src/rpg/fts/brownian/LMBdStep.cu
@@ -5,12 +5,39 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "LMBdStep.tpp"
+#include "LMBdStep.h"                     // class header
+#include <rpg/fts/brownian/BdSimulator.h>
+#include <rpg/fts/compressor/Compressor.h>
+#include <rpg/system/System.h>
+#include <rpg/solvers/Mixture.h>
+#include <rpg/field/Domain.h>
+#include <pscf/cuda/CudaVecRandom.h>
+#include <pscf/cuda/VecOp.h>
+
+#include <rp/fts/brownian/LMBdStep.tpp>   // base class implementation
 
 namespace Pscf {
 namespace Rpg {
-   template class LMBdStep<1>;
-   template class LMBdStep<2>;
-   template class LMBdStep<3>;
+
+   // Constructor.
+   template <int D>
+   LMBdStep<D>::LMBdStep(BdSimulator<D>& simulator)
+    : Rp::LMBdStep<D, Types<D> >(simulator)
+   {}
+
 }
+}
+
+// Explicit instantiation definitions
+namespace Pscf {
+   namespace Rp {
+      template class Rp::LMBdStep<1, Rpg::Types<1> >;
+      template class Rp::LMBdStep<2, Rpg::Types<2> >;
+      template class Rp::LMBdStep<3, Rpg::Types<3> >;
+   }
+   namespace Rpg {
+      template class LMBdStep<1>;
+      template class LMBdStep<2>;
+      template class LMBdStep<3>;
+   }
 }
