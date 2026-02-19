aeed24b6e0aa13194c3da8be05bc0ed2148731d4
chen7545 <chen7545@umn.edu>
2026-02-19 15:48:20 -0600

    Adding BoxLengthDerivative Analyzer

---
+++ src/rpg/fts/analyzer/BoxLengthDerivative.cu
@@ -0,0 +1,17 @@
+/*
+* PSCF - Polymer Self-Consistent Field
+*
+* Copyright 2015 - 2025, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "BoxLengthDerivative.tpp"
+
+namespace Pscf {
+namespace Rpg {
+   template class BoxLengthDerivative<1>;
+   template class BoxLengthDerivative<2>;
+   template class BoxLengthDerivative<3>;
+}
+}
+
