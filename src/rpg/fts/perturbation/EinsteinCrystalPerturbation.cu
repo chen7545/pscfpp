21c01ea87221f13091c979ff9d5938a38e108df8
David Morse <morse012@umn.edu>
2026-03-05 09:40:29 -0600

    Derive Rpg::EinsteinCrystalPerturbation from Rp template

--- src/rpg/fts/perturbation/EinsteinCrystalPerturbation.cu
+++ src/rpg/fts/perturbation/EinsteinCrystalPerturbation.cu
@@ -5,13 +5,39 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "EinsteinCrystalPerturbation.tpp"
+#include "EinsteinCrystalPerturbation.h"
+#include <rpg/fts/simulator/Simulator.h>
+#include <rpg/system/System.h>
+#include <rpg/solvers/Mixture.h>
+#include <rpg/field/Domain.h>
+#include <pscf/cuda/VecOp.h>
+#include <pscf/cuda/Reduce.h>
+
+#include <rp/fts/perturbation/EinsteinCrystalPerturbation.tpp>
 
 namespace Pscf {
 namespace Rpg {
 
-   template class EinsteinCrystalPerturbation<1>;
-   template class EinsteinCrystalPerturbation<2>;
-   template class EinsteinCrystalPerturbation<3>;
+   // Constructor.
+   template <int D>
+   EinsteinCrystalPerturbation<D>::EinsteinCrystalPerturbation(
+                                                  Simulator<D>& simulator)
+    : Rp::EinsteinCrystalPerturbation<D, Types<D> >(simulator)
+   {}
+
 }
+}
+
+// Explicit instantiation definitions
+namespace Pscf {
+   namespace Rp {
+      template class EinsteinCrystalPerturbation<1, Rpg::Types<1> >;
+      template class EinsteinCrystalPerturbation<2, Rpg::Types<2> >;
+      template class EinsteinCrystalPerturbation<3, Rpg::Types<3> >;
+   }
+   namespace Rpg {
+      template class EinsteinCrystalPerturbation<1>;
+      template class EinsteinCrystalPerturbation<2>;
+      template class EinsteinCrystalPerturbation<3>;
+   }
 }
--- src/rpg/fts/perturbation/EinsteinCrystalPerturbation.h
+++ src/rpg/fts/perturbation/EinsteinCrystalPerturbation.h
@@ -1,28 +1,32 @@
 #ifndef RPG_EINSTEIN_CRYSTAL_PERTURBATION_H
 #define RPG_EINSTEIN_CRYSTAL_PERTURBATION_H
 
-#include "Perturbation.h"            // base class
-#include <prdc/cuda/RField.h>        // member
-#include <util/containers/DArray.h>  // member
+#include <rp/fts/perturbation/EinsteinCrystalPerturbation.h> // base class
+#include <rpg/system/Types.h>                             // base argument
+#include <rpg/fts/perturbation/Perturbation.h>            // indirect base
+#include <prdc/cuda/RField.h>                             // base member
 
 namespace Pscf {
 namespace Rpg {
 
+   // Forward declaration
    template <int D> class Simulator;
-
-   using namespace Util;
-   using namespace Prdc;
-   using namespace Prdc::Cuda;
 
    /**
    * Perturbation for Einstein crystal thermodynamic integration.
+   *
+   * Instantiations of this template with D=1, 2, and 3 are derived from
+   * instantiations of the template Rp::EinsteinCrystalPerturbation, and
+   * inherit their public interface and almost all of their source code
+   * from this base class.  
    *
    * \see \ref rp_EinsteinCrystalPerturbation_page "Einstein Crystal"
    * \see \ref psfts_perturb_page "Perturbations"
    * \ingroup Rpg_Fts_Perturbation_Module
    */
    template <int D>
-   class EinsteinCrystalPerturbation : public Perturbation<D>
+   class EinsteinCrystalPerturbation 
+     : public Rp::EinsteinCrystalPerturbation< D, Types<D> >
    {
 
    public:
@@ -34,102 +38,22 @@ namespace Rpg {
       */
       EinsteinCrystalPerturbation(Simulator<D>& simulator);
 
-      /**
-      * Destructor.
-      */
-      virtual ~EinsteinCrystalPerturbation();
-
-      /**
-      * Read body of parameter file block and initialize.
-      *
-      * \param in  input parameter file stream
-      */
-      virtual void readParameters(std::istream& in);
-
-      /**
-      * Complete any required initialization.
-      */
-      virtual void setup();
-
-      /**
-      * Compute and return the perturbation to the Hamiltonian.
-      *
-      * \param unperturbedHamiltonian  Hamiltonian without perturbation
-      */
-      virtual double hamiltonian(double unperturbedHamiltonian);
-
-      /**
-      * Modify the generalized forces to include perturbation.
-      *
-      * \param dc  functional derivatives of Hamiltonian (in/out)
-      */
-      virtual void incrementDc(DArray< RField<D> >& dc);
-
-      /**
-      * Compute and return derivative of free energy w/ respect to lambda.
-      */
-      virtual double df();
-
-      /**
-      * Save any required internal state variables.
-      */
-      virtual void saveState();
-
-      /**
-      * Restore any required internal state variables.
-      */
-      virtual void restoreState();
-
-   protected:
-
-      // Inherited protected members
-      using Perturbation<D>::lambda_;
-      using Perturbation<D>::simulator;
-      using Perturbation<D>::system;
-      using ParamComposite::readOptionalDArray;
-
-   private:
-
-      // Parameters used in Einstein crystal integration
-      DArray<double> epsilon_;
-
-      // Reference w field
-      DArray< RField<D> > w0_;
-
-      // Eigenvector components of the reference w fields
-      DArray< RField<D> > wc0_;
-
-      // Work space
-      RField<D> dw_;
-
-      // Current Einstein crystal Hamiltonian
-      double ecHamiltonian_;
-
-      // Current unperturbed Hamiltonian
-      double unperturbedHamiltonian_;
-
-      // Saved Einstein crystal Hamiltonian
-      double stateEcHamiltonian_;
-
-      // Saved unperturbed Hamiltonian
-      double stateUnperturbedHamiltonian_;
-
-      // Reference field file name
-      std::string fieldFileName_;
-
-      // Have epsilon values been set?
-      bool hasEpsilon_;
-
-      // Compute eigenvector components of the reference field.
-      void computeWcReference();
-
    };
 
-   // Explicit instantiation declarations
-   extern template class EinsteinCrystalPerturbation<1>;
-   extern template class EinsteinCrystalPerturbation<2>;
-   extern template class EinsteinCrystalPerturbation<3>;
-
 }
+}
+
+// Explicit instantiation declarations
+namespace Pscf {
+   namespace Rp {
+      extern template class EinsteinCrystalPerturbation<1, Rpg::Types<1> >;
+      extern template class EinsteinCrystalPerturbation<2, Rpg::Types<2> >;
+      extern template class EinsteinCrystalPerturbation<3, Rpg::Types<3> >;
+   }
+   namespace Rpg {
+      extern template class EinsteinCrystalPerturbation<1>;
+      extern template class EinsteinCrystalPerturbation<2>;
+      extern template class EinsteinCrystalPerturbation<3>;
+   }
 }
 #endif
